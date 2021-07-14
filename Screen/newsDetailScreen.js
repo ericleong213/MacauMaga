919ddf3418ac7fbdb8fbe736030a27db98b390c6
Eric Leong <eric-213@hotmail.com>
2021-07-15 00:00:44 +0800

    news Detail

---
+++ Screen/newsDetailScreen.js
@@ -0,0 +1,67 @@
+import React, { useState } from "react";
+import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
+import { SimpleLineIcons, MaterialIcons } from "@expo/vector-icons";
+import NewsDetail from "../components/newsDetail";
+
+const NewsDetailScreen = (props) => {
+  const [selected, setSelected] = useState("news");
+
+  return (
+    <View style={styles.container}>
+      <View style={styles.header}>
+        <View style={styles.headerTop}>
+          <TouchableOpacity>
+            <SimpleLineIcons style={styles.iconArrow} name="arrow-left" />
+          </TouchableOpacity>
+          <Text style={styles.title}>快訊</Text>
+          <TouchableOpacity>
+            <MaterialIcons name="forward-to-inbox" style={styles.iconMail} />
+          </TouchableOpacity>
+        </View>
+      </View>
+      <View style={styles.subScreen}>
+        <NewsDetail />
+      </View>
+    </View>
+  );
+};
+
+export default NewsDetailScreen;
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: "#318b4d",
+    alignItems: "center",
+    height: "100%",
+  },
+  header: {
+    position: "absolute",
+    top: 35,
+    height: 75,
+  },
+  headerTop: {
+    display: "flex",
+    flexDirection: "row",
+  },
+  title: {
+    color: "white",
+    fontSize: 25,
+    marginHorizontal: 120,
+  },
+  iconArrow: {
+    color: "white",
+    fontSize: 25,
+  },
+  iconMail: {
+    color: "white",
+    fontSize: 25,
+  },
+
+  subScreen: {
+    position: "relative",
+    top: 75,
+    width: "100%",
+    height: 592,
+    backgroundColor: "white",
+  },
+});
---
+++ components/newsDetail.js
@@ -0,0 +1,56 @@
+import React, { useState } from "react";
+import { View, Text, StyleSheet, Image } from "react-native";
+
+const NewsDetail = (props) => {
+  return (
+    <View>
+      <View style={styles.titleContainer}>
+        <Text style={styles.titleTxt}>
+          海關辦比賽推廣保護知識產權7月起徵集作品
+        </Text>
+      </View>
+      <View style={styles.subTitleContainer}>
+        <Text style={styles.subTitleTxt}>
+          海關辦比賽推廣保護知識產權7月起徵集作品
+        </Text>
+      </View>
+      <View style={styles.subTitleContainer}>
+        <Image
+          style={styles.newsImg}
+          source={{
+            uri: "https://picsum.photos/200/200",
+          }}
+        />
+      </View>
+    </View>
+  );
+};
+
+export default NewsDetail;
+
+const styles = StyleSheet.create({
+  titleContainer: {
+    marginHorizontal: 20,
+    marginVertical: 10,
+    borderBottomWidth: 1,
+    borderColor: "black",
+    paddingBottom: 10,
+  },
+  titleTxt: {
+    fontSize: 20,
+    fontWeight: "500",
+  },
+  subTitleContainer: {
+    marginHorizontal: 20,
+    marginVertical: 0,
+  },
+  subTitleTxt: {
+    fontSize: 16,
+    color: "grey",
+  },
+  newsImg: {
+    marginVertical: 10,
+    width: 330,
+    height: 480,
+  },
+});
